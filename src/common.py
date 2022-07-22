a7f62d98c0f0f81ef3c77a9497dbd07e0252625b
dantkv <dantkv@outlook.com>
2022-07-22 22:06:10 +0800

    refactor: 重构用户数据枚举类
    - 移除 UserDataEnum 类变量
    - 修改函数 getUID 为静态函数

--- src/common.py
+++ src/common.py
@@ -16,37 +16,37 @@ class CommonEnum(Enum):
     # 系统用户变量
     USERPROFILE = os.environ["USERPROFILE"]
 
+
 class UserDataEnum():
     """
     用户数据存储路径枚举
     """
-    UID:str = ""
-    # 用户数据根目录
-    USER_DATA_PATH:str = ""
-    # 抽卡记录文件路径
-    GACHA_DATA_FILE_PATH:str = ""
-    # 统计结果路径
-    RESULT_FILE_PATH:str = ""
-    # 配置文件路径
-    CONFIG_FILE_PATH:str = ""
 
-    def __init__(self, uid) -> None:
+    def __init__(self, uid = None) -> None:
         self.update(uid)
 
     def update(self, uid):
-        self.UID= uid
+        if not uid:
+            self.UID = UserDataEnum.getUID()
+        else:
+            self.UID = uid
+        # 用户数据根目录
         self.USER_DATA_PATH = os.path.join(CommonEnum.DATA_PATH.value, str(self.UID))
+        # 抽卡记录文件路径
         self.GACHA_DATA_FILE_PATH = os.path.join(self.USER_DATA_PATH, "gacha_data.json")
+        # 统计结果路径
         self.RESULT_FILE_PATH = os.path.join(self.USER_DATA_PATH, "result.json")
+        # 配置文件路径
         self.CONFIG_FILE_PATH = os.path.join(self.USER_DATA_PATH, "config.json")
 
-def getUID():
-    """
-    从游戏文件获取UID
-    """
-    uid_info_file = os.path.join(CommonEnum.USERPROFILE.value, "AppData", "LocalLow", "miHoYo", "原神", "UidInfo.txt")
-    with open(uid_info_file, "r", encoding="utf-8", errors="ignore") as f:
-        uid = f.readline().rstrip()
-    return uid
+    @staticmethod
+    def getUID():
+        """
+        从游戏文件获取UID
+        """
+        uid_info_file = os.path.join(CommonEnum.USERPROFILE.value, "AppData", "LocalLow", "miHoYo", "原神", "UidInfo.txt")
+        with open(uid_info_file, "r", encoding="utf-8", errors="ignore") as f:
+            uid = f.readline().rstrip()
+        return uid
 
-USER_DATA_ENUM = UserDataEnum(getUID())
+USER_DATA_ENUM = UserDataEnum()
